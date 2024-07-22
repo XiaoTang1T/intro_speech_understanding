ae216a71dfc9a1af6dfa02d744ba96c0e4ff6628
XiaoTang1T <166464631+XiaoTang1T@users.noreply.github.com>
2024-07-23 01:17:46 +0900

    I did homework13
    I did homework13

--- 2024_spring/lec13/grade.py
+++ 2024_spring/lec13/grade.py
@@ -1,7 +1,5 @@
-import unittest
-import pathlib
-import json
-from contextlib import contextmanager, redirect_stderr, redirect_stdout
+import unittest, pathlib, json
+from contextlib import contextmanager,redirect_stderr,redirect_stdout
 from os import devnull
 
 @contextmanager
@@ -11,12 +9,14 @@ def suppress_stdout_stderr():
         with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
             yield (err, out)
 
+
+# TestSequence
 class Test(unittest.TestCase):
     def import_homework13(self):
         try:
             import homework13
             self.homework13 = homework13
-        except ImportError:
+        except:
             self.fail("You did not upload a text file called homework13.py!")
 
     def extract_stories_from_NPR_text(self, webpage_text):
@@ -25,84 +25,28 @@ class Test(unittest.TestCase):
             return stories
         except AttributeError:
             self.fail("homework13.py does not have a method called extract_stories_from_NPR_text!")
-
+    
     def read_nth_story(self, stories, n, filename):
         try:
             self.homework13.read_nth_story(stories, n, filename)
         except AttributeError:
             self.fail("homework13.py does not have a method called read_nth_story!")
-
+    
     def assertIsFile(self, path):
         if not pathlib.Path(path).resolve().is_file():
             raise AssertionError("File does not exist: %s" % str(path))
 
     def test_extract_stories_from_NPR_text_exists(self):
         self.import_homework13()
-        with open('npr_webpage.html', 'r', encoding='utf-8') as f:
+        with open('npr_webpage.html') as f:
             webpage_text = f.read()
         self.extract_stories_from_NPR_text(webpage_text)
         self.assertTrue("homework13.py contains a method called 'extract_stories_from_NPR_text'")
-
+       
     def test_read_nth_story_exists(self):
         self.import_homework13()
-        with open('stories.json', 'r', encoding='utf-8') as f:
+        with open('stories.json') as f:
             stories = json.load(f)
         self.read_nth_story(stories, 3, 'test.mp3')
         self.assertTrue("homework13.py contains a method called 'read_nth_story'")
-
-    def test_extract_stories_from_NPR_text_returns_list_of_strings(self):
-        self.import_homework13()
-        with open('npr_webpage.html', 'r', encoding='utf-8') as f:
-            webpage_text = f.read()
-        stories = self.extract_stories_from_NPR_text(webpage_text)
-        self.assertIsInstance(stories, list, 'extract_stories_from_NPR_text should return a list')
-        self.assertGreater(len(stories), 0, 'extract_stories_from_NPR_text should not be zero-length')
-        self.assertIsInstance(stories[0], tuple, 'extract_stories_from_NPR_text should be list of tuples')
-        self.assertIsInstance(stories[0][0], str, 'extract_stories_from_NPR_text should be list of tuples of strings')
-
-    def test_extract_stories_from_NPR_text_returns_correct_list(self):
-        self.import_homework13()
-        with open('npr_webpage.html', 'r', encoding='utf-8') as f:
-            webpage_text = f.read()
-        with open('stories.json', 'r', encoding='utf-8') as f:
-            ref = json.load(f)
-        stories = self.extract_stories_from_NPR_text(webpage_text)
-        self.assertIsInstance(stories, list, 'extract_stories_from_NPR_text should return a list')
-        self.assertGreater(len(stories), 0, 'extract_stories_from_NPR_text should not be zero-length')
-        self.assertIsInstance(stories[0], tuple, 'extract_stories_from_NPR_text should be list of tuples')
-        self.assertIsInstance(stories[0][0], str, 'extract_stories_from_NPR_text should be list of tuples of strings')
-        self.assertEqual(stories[0][0], ref[0][0],
-                         '''
-                         extract_stories_from_NPR_text should return %s, instead it returns %s
-                         ''' % (ref[0][0], stories[0][0]))
-
-    def test_read_nth_story_creates_file(self):
-        self.import_homework13()
-        with open('stories.json', 'r', encoding='utf-8') as f:
-            stories = json.load(f)
-        self.read_nth_story(stories, 3, 'test.mp3')
-        self.assertIsFile("test.mp3")
-
-    def test_read_nth_story_creates_correct_synthesis(self):
-        self.import_homework13()
-        with open('stories.json', 'r', encoding='utf-8') as f:
-            stories = json.load(f)
-        self.read_nth_story(stories, 3, 'test.mp3')
-        with open("test.mp3", "rb") as f:
-            hypothesis = f.read()
-        with open("solution.mp3", "rb") as f:
-            reference = f.read()
-        self.assertEqual(int(len(hypothesis) / 1024), int(len(reference) / 1024),
-                         '''
-                         read_nth_story creates a file with the wrong content.
-                         '''
-        )
-
-suite = unittest.defaultTestLoader.loadTestsFromTestCase(Test)
-result = unittest.TextTestRunner().run(suite)
-
-n_success = result.testsRun - len(result.errors) - len(result.failures)
-print('%d successes out of %d tests run' % (n_success, result.testsRun))
-print('Score: %d%%' % (int(100 * (n_success / result.testsRun))))
-
-
+    
